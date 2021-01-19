ed430c9f866d63dbf88edc9c54c9c326eb2065e9
Jacky Huang <jacky_huang56@hotmail.com>
2021-01-18 17:48:20 -0700

    got code from eclass

--- client.py
+++ client.py
@@ -1,36 +1,38 @@
+#!/usr/bin/env python3
 import socket, sys
 
 #create a tcp socket
 def create_tcp_socket():
-    print("Creating socket")
+    print('Creating socket')
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except (socket.error, msg):
-        print(f'Failed to create socket. Error code: {str(msg[0])}, Error message: {msg[1]}')
+        print(f'Failed to create socket. Error code: {str(msg[0])} , Error message : {msg[1]}')
         sys.exit()
-    print("Socket created succesfully")
+    print('Socket created successfully')
     return s
 
 #get host information
 def get_remote_ip(host):
-    print(f"Getting IP for {host}")
+    print(f'Getting IP for {host}')
     try:
-        remote_ip = socket.gethostbyname(host)
+        remote_ip = socket.gethostbyname( host )
     except socket.gaierror:
-        print('Hostname could not be resolved. Exciting')
+        print ('Hostname could not be resolved. Exiting')
         sys.exit()
-    print(f'IP address of {host} is {remote_ip}')
+
+    print (f'Ip address of {host} is {remote_ip}')
     return remote_ip
 
 #send data to server
 def send_data(serversocket, payload):
-    print('Sending payload')
+    print("Sending payload")    
     try:
         serversocket.sendall(payload.encode())
     except socket.error:
-        print('send failed')
+        print ('Send failed')
         sys.exit()
-    print("Payload sent sucessfully")
+    print("Payload sent successfully")
 
 def main():
     try:
@@ -44,27 +46,27 @@ def main():
         s = create_tcp_socket()
 
         remote_ip = get_remote_ip(host)
-        
-        s.connect((remote_ip, port))
-        print(f'Socket connected to {host} on ip {remote_ip}')
 
+        s.connect((remote_ip , port))
+        print (f'Socket Connected to {host} on ip {remote_ip}')
+        
         #send the data and shutdown
         send_data(s, payload)
         s.shutdown(socket.SHUT_WR)
 
-        #continue accepting data unitl no more left
+        #continue accepting data until no more left
         full_data = b""
         while True:
             data = s.recv(buffer_size)
             if not data:
-                break
+                 break
             full_data += data
         print(full_data)
     except Exception as e:
         print(e)
     finally:
-        #always close at the end
+        #always close at the end!
         s.close()
-
 if __name__ == "__main__":
     main()
+
