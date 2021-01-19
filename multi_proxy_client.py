b487e29c299cee9c8ee23487920e25a0c33ffdde
JackyH56 <44718684+JackyH56@users.noreply.github.com>
2021-01-18 21:36:56 -0700

    Delete multi_proxy_client.py

--- multi_proxy_client.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import Pool
-def main():
-    with Pool() as p:
-        p.map(connect, address * 10)
-
-        p = Process(target=handle_echo, args=(addr,conn))
-        p.daemon()
-        p.start()
